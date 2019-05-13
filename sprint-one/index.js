7e4d038b5d2f741d45bf9b451d0dfbcfec7a82e1
equiman <equiman@users.noreply.github.com>
2019-05-13 00:58:11 -0500

    Add default command, strict and demand

--- sprint-one/index.js
+++ sprint-one/index.js
@@ -33,7 +33,9 @@ const argsSubscribe = {
 };
 
 const argv = require('yargs')
-  .command('courses', 'Show available courses list', argsCourse, ({ time }) => course.list(time))
+  .command(['courses', '$0'], 'Show available courses list', argsCourse, ({ time }) => course.list(time))
   .usage('Usage: $0 -time=[num in seconds]')
-  .command('subscribe', 'Subscribe an student in course', argsSubscribe, ({ id, document, name }) => subscribe.add(id, { document, name }))
-  .usage('Usage: $0 -course=[id] -document=[num] -name=[string]').argv;
+  .command('subscribe', 'Subscribe student in course', argsSubscribe, ({ id, document, name }) => subscribe.add(id, { document, name }))
+  .usage('Usage: $0 -course=[id] -document=[num] -name=[string]')
+  .strict()
+  .demandCommand().argv;
